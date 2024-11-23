f5f442ebb4c5866bc3c47fa28fa6d853b4fcd774
Andrew Ohwoka <150045330+AndrewOhwoka@users.noreply.github.com>
2024-11-23 15:07:45 -0330

    members commit

---
+++ src/main/java/com/keyin/members/MemberRepository.java
@@ -0,0 +1,13 @@
+
+package com.keyin.members;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+@RepositoryRestResource(exported = false)
+public interface MemberRepository extends CrudRepository<Member, Long> {
+
+    public Member findByName(String name);
+
+}
